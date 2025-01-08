90d45392978d1c0b908973cd42981d543484076c
Wahyu Bagus Pambudi <wb84222@gmail.com>
2025-01-08 09:15:27 +0700

    feat: :zap: `Refactor layout and components, update styles and scripts`

--- components/organisms/Layout/index.tsx
+++ components/organisms/Layout/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Nav from "../Nav";
+import Sidebar from "../Sidebar";
 
 export default function Layout() {
-  return <div>Layout</div>;
+  return (
+    <>
+      <div className="sb-nav-fixed">
+        <Nav />
+        <div id="layoutSidenav">
+          <Sidebar />
+        </div>
+      </div>
+    </>
+  );
 }
